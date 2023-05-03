4cba1d8c630776a75719492700357a4ed268eff3
KaremVPintoF <117313230+KaremVPintoF@users.noreply.github.com>
2023-05-02 20:45:39 -0500

    39 mejoras de presentación de la tabla de clasificación (#45)
    * Cambios en la presentación de la tabla
    
    * Cambios en presentación de la tabla
    
    * Actualización de formato de porcentajes
    
    * la URI del servidor retorna tipo de datos más correcto
    
    * Hora y fecha y porcentajes
    
    ---------
    
    Co-authored-by: Kapfva <kapfva@gmail.com>
    Co-authored-by: edwin-sirenaai <navarrete_edwin@yahoo.com.mx>

--- statClient/src/components/table.tsx
+++ statClient/src/components/table.tsx
@@ -10,7 +10,6 @@ import Paper from '@mui/material/Paper';
 import Tooltip from '@mui/material/Tooltip';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import useSort from '../hooks/useSort';
 import { Divider } from '@mui/material';
 import dayjs from 'dayjs';
 import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
@@ -20,19 +19,52 @@ import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 
 const today = dayjs();
-//
 
 interface IData {
 
   // define la estructura de los datos que esperas recibir de la API
-  album_id: string; // En 
+  album_id: string; // En
   error_number: number;
   answered_question_number: number;
   ended_album: string;
   error_percentage: number;
   total_response_time: number;
   epocas: number;
+  rank:number;
 
+}
+
+function sortArray (dataToSort:any) {
+  if (dataToSort.length > 0) {
+    // setDataSorted(dataToSort);
+   dataToSort.sort(function(a: any,b:any) {
+        //Sort by less time
+        return a.total_response_time - b.total_response_time;
+    })
+   dataToSort.sort(function(a: any,b:any) {
+        //Sort by status of album (enden or not)
+        if (a.ended_album > b.ended_album) {
+            return -1;
+        }
+
+        if (a.ended_album < b.ended_album) {
+            return 1;
+        }
+        // //Sort by less error number
+        return a.error_number - b.error_number ;
+    })
+  }
+  return addRank(dataToSort)
+}
+//Método que añade posiciones
+function addRank(arrayData:any) {
+  let position = 1
+  for (let index = 0; index < arrayData.length; index++) {
+    const element = arrayData[index];
+    element.rank = position++;
+  }
+  // console.log(arrayData)
+  return arrayData
 }
 
 
@@ -44,36 +76,32 @@ export default function BasicTable() {
     setSelectedDate(date);
   };
 
-  // Hooks for query data from server API 
+  // Hooks for query data from server API
   const [data, setData] = useState<IData[]>([]);
 
   useEffect(() => {
     axios
-      .get("http://localhost:8001/user")//Url api server
+      .get("http://localhost:3004/user")//Url api server
       .then((res) => {
-        setData(res.data.data);
+        setData(sortArray(res.data.data));
 
       })
       .catch((error) => {
         console.log(error);
       });
-
   }, []);
-
-  //Use of hook useSort to 
-  const dataSorted = useSort(data);
-  // console.log(dataSorted)
 
   // Filter data to show only rows with matching dates
   const filteredData = data.filter(row => {
     const rowDate = dayjs(row.epocas);
-    if (selectedDate == today) {
+    if (selectedDate === today) {
       return rowDate
     }
     else {
 
       return rowDate.isSame(selectedDate, 'day');
     }
+
   });
   ;
 
@@ -122,13 +150,14 @@ export default function BasicTable() {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Album Id</TableCell>
-            <TableCell align="right">Número de errores</TableCell>
-            <TableCell align="right">Preguntas respondidas</TableCell>
+          <TableCell>Posición</TableCell>
+            <TableCell align="right">Album</TableCell>
+            <TableCell align="right">Fecha y hora de inicio</TableCell>
             <TableCell align="right">Album finalizado</TableCell>
-            <TableCell align="right">Tiempo total de respuesta (s)</TableCell>
+            <TableCell align="right">Preguntas respondidas</TableCell>
+            <TableCell align="right">Número de errores</TableCell>
             <TableCell align="right">Porcentaje de error (%)  </TableCell>
-            <TableCell align="right">Fecha</TableCell>
+            <TableCell align="right">Tiempo total de respuesta (Segundos)</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -138,16 +167,20 @@ export default function BasicTable() {
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
+                {row.rank}
+              </TableCell>
+              <TableCell align="right" >
                 {row.album_id.slice(-5)}
               </TableCell>
-              <TableCell align="right">{row.error_number}</TableCell>
-              <TableCell align="right">{row.answered_question_number}</TableCell>
-              <TableCell align="right">{row.ended_album}</TableCell>
-              <TableCell align="right">{row.error_percentage}</TableCell>
-              <TableCell align="right">{row.total_response_time}</TableCell>
               <TableCell align="right">{
-                row.epocas ? new Date(row.epocas).toLocaleDateString() : new Date().toLocaleDateString()
+                new Date(row.epocas).toLocaleString()
               }</TableCell>
+              <TableCell align="right">{row.ended_album? "Sí" : "No"}</TableCell>
+              <TableCell align="right">{row.answered_question_number}</TableCell>
+              <TableCell align="right">{row.error_number}</TableCell>
+              <TableCell align="right">{row.error_percentage*100}%</TableCell>
+              <TableCell align="right">{row.total_response_time}</TableCell>
+
             </TableRow>
           ))}
         </TableBody>
