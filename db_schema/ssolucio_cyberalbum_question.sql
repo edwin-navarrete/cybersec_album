a5d792795cf8b2f4820cdb31655ba37cfd8a52f7
KaremVPintoF <karemvalentina.pinto385@comunidadunir.net>
2023-05-22 17:55:10 -0500

    Sql para creación de tabla question

--- db_schema/ssolucio_cyberalbum_question.sql
+++ db_schema/ssolucio_cyberalbum_question.sql
@@ -34,15 +34,6 @@ CREATE TABLE `question` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `question`
---
-
-LOCK TABLES `question` WRITE;
-/*!40000 ALTER TABLE `question` DISABLE KEYS */;
-/*!40000 ALTER TABLE `question` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +44,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-22 17:50:36
+-- Dump completed on 2023-05-22 17:53:29
