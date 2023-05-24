11e441128ef2699e5e0bc402ca356492ab253fa0
jcalderonagudelo <85127168+jcalderonagudelo@users.noreply.github.com>
2023-05-24 17:36:05 -0500

    Añadiendo filtro por fecha desde consulta a data base. Resolviendo #53 (#57)
    * Añadiendo filtro por fecha desde consulta a data base
    
    * Aplicando correcciones de pull request #57
    
    Se realizan las correcciones solicitadas en este PR.

--- statClient/src/index.tsx
+++ statClient/src/index.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Header from './components/header';
-import EnhancedTable  from './components/table';
-
 
 
 
