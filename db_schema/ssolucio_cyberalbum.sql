188ae9abe5462d0eeab27e588aa16a979494e9cc
jcalderonagudelo <85127168+jcalderonagudelo@users.noreply.github.com>
2023-06-05 20:32:33 -0500

    Resolviendo issue número #54 se necesitan la metadata de las preguntas en la bd (#60)
    * Sql para creación de tabla question
    
    * Sql para creación de tabla question
    
    * Sql para creación de tabla question con cambios
    
    * SQL de datos de prueba
    
    * Eliminando sql de prueba para tabla question
    
    Se elimina del pull request con el fin de actualizar información según documento
    
    * SQL con preguntas extraídas de questionDB.json
    
    * SQL con preguntas extraídas de questionDB.json
    
    * Solucionando issue #54 "Se necesitan la metadata de las preguntas en la BD #54"
    
    Se agregan las consultas respectivas al archivo cybersec_album\db_schema\ssolucio_cyberalbum.sql también se hacen cambios en los archivos  cybersec_album\client\src\features\game\data\en\questionDB.json y cybersec_album\client\src\features\game\data\es\questionDB.json ya que había un error en uno de los tipos de la pregunta, se cambia de single a multiple
    
    * Borrando db_schema\ssolucio_cyberalbum_question.sql
    
    Se borra db_schema\ssolucio_cyberalbum_question.sql ya que la solución se pide es en el archivo db_schema\ssolucio_cyberalbum.sql
    
    * Se elimina db_schema\sample_data_question.sql
    
    Para evitar confusiones al desarrollar se elimina el archivo db_schema\sample_data_question.sql
    
    * Cambios en db_schema\ssolucion_cyberalbum.sql ineas 106 y lineas 109 a 112
    
    ---------
    
    Co-authored-by: KaremVPintoF <karemvalentina.pinto385@comunidadunir.net>
    Co-authored-by: KaremVPintoF <117313230+KaremVPintoF@users.noreply.github.com>

--- db_schema/ssolucio_cyberalbum.sql
+++ db_schema/ssolucio_cyberalbum.sql
@@ -92,3 +92,68 @@ CREATE TABLE `user_sticker` (
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-04-30  9:13:30
+
+-- Crear la tabla question
+
+CREATE TABLE question (
+  id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  type VARCHAR(10) NOT NULL,
+  lang VARCHAR(10) NOT NULL,
+  question TEXT NOT NULL,
+  options JSON NOT NULL,
+  solution JSON NOT NULL,
+  difficulty FLOAT NOT NULL,
+  feedback TEXT
+);
+
+
+-- Insertando las preguntas en español
+INSERT INTO question (type, lang, question, options, solution, difficulty, feedback) VALUES
+('single', 'ES', '¿Qué es seguridad?', '["Relativo a confianza", "Lo contrario de inseguridad", "Necesidad del ser humano", "Todas las anteriores"]', '[3]', 0.4, 'Recuerda que seguridad en el sentido más amplio incluye la protección de miembros de un grupo contra daños así como la percepción individual del riesgo.'),
+('single', 'ES', '¿Qué es seguridad de información?', '["Asegurar los equipos informáticos", "Asegurar los documentos de la empresa", "Asegurar los activos de información", "Asegurar la información digital"]', '[2]', 0.3, NULL),
+('single', 'ES', 'La ciberseguridad protege información en:', '["Formato digital y sistemas interconectados", "Formatos interconectados y sistemas digitales", "Ciberespacio", "Espacio Cibernético"]', '[0]', 0.4, NULL),
+('multiple', 'ES', '¿Cuál de estos son activos de información?', '["Pagina web", "Correos electrónicos de clientes", "Puerta del Centro de Cómputo", "Documentación escrita"]', '[0, 1, 3]', 0.4, NULL),
+('multiple', 'ES', 'Las dimensiones de la seguridad de información son:', '["Disponibilidad", "Ubicuidad", "Integridad", "Confidencialidad"]', '[0, 2, 3]', 0.3, NULL),
+('single', 'ES', 'La fuga de información en una entidad sería un aspecto a cubrir por:', '["Integridad", "Confidencialidad", "Disponibilidad", "Calidad"]', '[1]', 0.5, NULL),
+('single', 'ES', 'Cuál de estos pilares, NO es de seguridad de información:', '["Disponibilidad", "Unidad", "Integridad", "Confidencialidad"]', '[1]', 0.4, NULL),
+('single', 'ES', 'Un empleado descontento constituye para la organización', '["Un riesgo", "Una vulnerabilidad", "Una amenaza", "Un control"]', '[2]', 0.6, NULL),
+('single', 'ES', 'Seguridad de la información abarca', '["Seguridad Informática", "Seguridad Informática y Ciberseguridad", "Ciberseguridad", "Seguridad Cibernética"]', '[1]', 0.5, NULL),
+('single', 'ES', 'Ataque informático especifícamente dirigido a líderes o altos directivos', '["Whaling", "Vishing", "Phishing", "Pharming"]', '[0]', 0.5, NULL),
+('multiple', 'ES', 'Cuáles son los impactos que puede causar un ataque informático en una entidad?', '["Financiero", "Reputacional", "Legal", "Salud"]', '[0, 1, 2]', 0.6, NULL),
+('single', 'ES', '¿Cuáles son las técnicas de influencia y manipulación utilizadas por los adversarios?', '["Autoridad, Empatía", "Concesión, Urgencia ", "Reciprocidad, Lenguaje no verbal", "Todas las anteriores"]', '[3]', 0.6, NULL),
+('single', 'ES', '¿Cuál NO es una forma de protección?', '["Entrada a enlaces sospechosos", "Copias de Seguridad", "Contraseñas seguras", "Doble factor de autenticación"]', '[0]', 0.4, NULL),
+('single', 'ES', 'Una mala práctica al hacer la copias de seguridad de su información es:', '["Hacer copias incrementales de manera periódica", "Guardarlas en dos lugares diferentes dentro del mismo edificio", "Seleccionar solo la información relevante a copiar", "Guardar la copia en una sede alterna"]', '[1]', 0.6, NULL),
+('single', 'ES', 'Las políticas de seguridad de información son:', '["Directrices para proteger la información", "El catálogo de activos de información", "La lista de los riesgos de la entidad", "Los documentos que nadie conoce"]', '[0]', 0.6, NULL),
+('single', 'ES', 'Un error en una contraseña segura es:', '["Una combinación de letras, números y caracteres", "Usar una frase de película o poema preferido", "Tener información personal de fácil recordación", "Tener una longitud de 12 caracteres"]', '[2]', 0.6, NULL),
+('single', 'ES', 'Los riesgos pueden dividirse en dos grandes categorías:', '["Ambientales y humanos", "Virus y phishing", "Pérdida de información y suplantación de identidad", "Entrada y salida de equipos"]', '[0]', 0.5, NULL),
+('single', 'ES', 'Un dato personal público es aquel que:', '["Es de naturaleza íntima, solo le interesa al titular", "Es de interés para el titular y para un sector específico", "Con su uso indebido se puede generar discriminación", "Puede ser consultado por cualquiera sin el consentimiento del titular"]', '[3]', 0.6, NULL),
+('single', 'ES', 'Es un ejemplo de dato sensible:', '["El nombre", "Información de salud", "El número telefónico", "Fecha y lugar de nacimiento"]', '[1]', 0.6, NULL),
+('single', 'ES', 'Una amenaza física de factor humano:', '["Robo de información", "Daño de equipo por cambio de voltaje", "Inundación de centro de datos", "Terremoto"]', '[0]', 0.7, NULL),
+('multiple', 'ES', 'Son clases de amenazas:', '["Deliberada, Accidental", "Imaginarias, reales", "Internas, Externas", "Ambientales"]', '[0, 1, 3]', 0.7, NULL),
+('multiple', 'ES', 'Son clases de vulnerabilidades:', '["Deliberada, Accidental ", "Red, Lugar", "Hardware, Software", "Personal, Organización"]', '[1, 2, 3]', 0.7, NULL);
+
+
+-- Insertando las preguntas en inglés
+INSERT INTO question (type, lang, question, options, solution, difficulty, feedback) VALUES
+('single', 'EN', 'What is security?', '["Related to trust", "The opposite of insecurity", "Necessity of the human being", "All of the above"]', '[3]', 0.4, 'Remember that security in the broadest sense includes the protection of members of a group from harm as well as individual perception of risk.'),
+('single', 'EN', 'What is information security?', '["Securing computer equipment", "Securing company documents", "Securing information assets", "Securing digital information"]', '[2]', 0.5, NULL),
+('single', 'EN', 'Cybersecurity protects information in:', '["Digital format and interconnected systems", "Interconnected formats and digital systems", "Cyberspace", "Physical"]', '[0]', 0.7, NULL),
+('multiple', 'EN', 'Which of these are information assets?', '["Web page", "Customer Emails", "Door of the Computing Center", "Written Documentation"]', '[0, 1, 3]', 0.8, NULL),
+('multiple', 'EN', 'The dimensions of information security are:', '["Availability", "Ubiquity", "Integrity", "Confidentiality"]', '[0, 2, 3]', 0.8, NULL),
+('single', 'EN', 'The leak of information in an corporation would be an aspect to cover by:', '["Integrity", "Confidentiality", "Availability", "Quality"]', '[1]', 0.6, NULL),
+('single', 'EN', 'Which of these pillars is NOT information security:', '["Availability", "Unit", "Integrity", "Confidentiality"]', '[1]', 0.4, NULL),
+('single', 'EN', 'A dissatisfied employee constitutes for the organization', '["A risk", "A vulnerability", "A threat", "A control"]', '[2]', 0.7, NULL),
+('single', 'EN', 'Information security encompasses', '["Information Security", "Information Security and Cybersecurity", "Cybersecurity", "Security"]', '[2]', 0.7, NULL),
+('single', 'EN', 'Computer attack specifically targeting leaders or senior managers', '["Whaling", "Vishing", "Phishing", "Pharming"]', '[0]', 0.8, NULL),
+('multiple', 'EN', 'What are the impacts that a computer attack can cause in a corporation?', '["Financial", "Reputational", "Legal", "Health"]', '[0, 1, 2]', 0.8, NULL),
+('single', 'EN', 'What are the techniques of influence and manipulation used by adversaries?', '["Authority, Empathy", "Concession, Urgency", "Reciprocity, Nonverbal Language", "All of the above"]', '[3]', 0.9, NULL),
+('single', 'EN', 'Which is NOT a form of protection?', '["Entry to suspicious links", "Backups", "Secure Passwords", "Double authentication factor"]', '[0]', 0.4, NULL),
+('single', 'EN', 'A bad practice when backing up your data is:', '["Make incremental copies periodically", "Save them in two different places within the same building", "Select only the relevant information to copy", "Save the copy in an alternate location"]', '[1]', 0.7, NULL),
+('single', 'EN', 'Information security policies are:', '["Guidelines to protect information", "The catalog of Information Assets", "The list of risks of the corporation", "The documents nobody knows"]', '[0]', 0.6, NULL),
+('single', 'EN', 'An error in a strong password is:', '["A combination of letters, numbers and characters", "Use a phrase from a favorite movie or poem", "Have easy-to-remember personal information", "Be 12 characters long"]', '[2]', 0.7, NULL),
+('single', 'EN', 'Risks can be divided into two broad categories:', '["Environmental and human", "Viruses and Phishing", "Loss of information and identity theft", "Input and output of equipment"]', '[0]', 0.6, NULL),
+('single', 'EN', 'A public personal data is that which:', '["It is of an intimate nature, it only interests the holder", "It is of interest for the holder and for a specific sector", "With its improper use, discrimination can be generated", "It can be consulted by anyone without the consent of the owner"]', '[3]', 0.7, NULL),
+('single', 'EN', 'This is an example of sensitive data:', '["Name", "Health Information", "The phone number", "Date and place of birth"]', '[1]', 0.6, NULL),
+('single', 'EN', 'A human factor physical threat:', '["Information theft", "Equipment damage due to voltage change", "Data Center flood", "Earthquake"]', '[0]', 0.7, NULL),
+('multiple', 'EN', 'Threat classes:', '["Deliberate, Accidental", "Imaginary, real", "Internal, External", "Environmental"]', '[0, 1, 3]', 0.9, NULL),
+('multiple', 'EN', 'These are vulnerability classes:', '["Deliberate, Accidental", "Network, Place", "Hardware,Software", "Staff, Organization"]', '[1, 2, 3]', 0.9, NULL);
