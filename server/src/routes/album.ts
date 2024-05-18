18b929fe10e7f4d23149edec254a386e7d3b83b8
edwin-navarrete <navarrete_edwin@yahoo.com.mx>
2024-05-18 17:06:36 -0500

    Moving server code to src

--- server/src/routes/album.ts
+++ server/src/routes/album.ts
@@ -53,6 +53,7 @@ router.post('/album', [
   validateInput
 ], async (req: Request, res: Response) => {
   const dao = new AlbumDAO(mysqlDriver.fetch, mysqlDriver.insert, 'album')
+  console.log(req)
   const value = {
     album_id: req.body.albumId,
     player_name: req.body.playerName || null,
