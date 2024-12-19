6b3e0b3bcc89ff1f955829ff18c3cad40c6877f0
edwin-navarrete <navarrete_edwin@yahoo.com.mx>
2024-12-19 11:08:26 -0500

    Fixed static resources ordering

--- server/src/models/server.ts
+++ server/src/models/server.ts
@@ -11,13 +11,15 @@ export class Server {
   constructor () {
     this.port = process.env.PORT || '5006' // Loaded from .env file
     this.middlewares()
+    
+    // this.app.use('/cybersec_album', express.static(path.join(__dirname, '../client/build')))
+    this.routes()
 
+    // Serve the React app here
     this.app.use(express.static(path.join(__dirname, '../../../client/build')))
     this.app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../../../client/build', 'index.html'));
     });
-    // this.app.use('/cybersec_album', express.static(path.join(__dirname, '../client/build')))
-    this.routes()
   }
 
   middlewares () {
